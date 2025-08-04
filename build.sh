a34a4dccf744f877ed6b5ec4a18b866cab8ac0f9
zaiskydna <zakypati221@gmail.com>
2025-08-04 20:52:06 +0700

    bismillah

--- build.sh
+++ build.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-# Membangun file statis
+# 1. Install semua library dari requirements.txt
+pip install -r requirements.txt
+
+# 2. Kumpulkan file statis
 python3 manage.py collectstatic --noinput
 
-# Menjalankan migrasi database
+# 3. Jalankan migrasi database
 python3 manage.py migrate
